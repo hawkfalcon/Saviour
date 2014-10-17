82ead6c99cb19894dd4e8cd0d5905cb73aafcf89
hawkfalcon <hawkfalcon1@gmail.com>
2014-10-17 14:50:32 -0700

    Added Acid - Leighton
    Acid burninates the player

---
+++ Acid.java
@@ -0,0 +1,12 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Acid
+ */
+public class Acid extends Obstacle
+{
+   public void act() 
+    {
+        killer();
+    }    
+}
