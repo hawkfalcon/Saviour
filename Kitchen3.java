17b45cc58605f42e4cb21b3e3de38baa5b255b9b
Dane Low <dnlow@live.com>
2014-10-28 09:34:56 -0700

    Kitchens Level 1
    Wall Construction

---
+++ Kitchen3.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Kitchen3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Kitchen3 extends SaviourLevel
+{
+
+    /**
+     * Constructor for objects of class Kitchen3.
+     * 
+     */
+    public Kitchen3()
+    {
+        super(90, 70, 675, 200);
+    }
+}
