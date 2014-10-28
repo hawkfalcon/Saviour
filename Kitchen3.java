3c75a225e42595d1d100663152f2c428df671d8d
Dane Low <dnlow@live.com>
2014-10-28 09:56:25 -0700

    Kitchen 3
    Kitchen 3 Wall construction

--- Kitchen3.java
+++ Kitchen3.java
@@ -15,6 +15,50 @@ public class Kitchen3 extends SaviourLevel
      */
     public Kitchen3()
     {
-        super(90, 70, 675, 200);
+        super(90, 70, 825, 200);
+        prepareWalls();
+    }
+    
+    public void prepareWalls()
+    {
+        //generates all the walls. 
+        //you could just place them and save the world.
+        
+        //Top Left Horizontal Wall
+         for (int i = 1; i < 10; i++) {
+            addObject(new Wall(), getPos(i), (getHeight() / 4) * 1 - halfWall);
+        }
+        //Top Right Horizontal Wall
+        for (int i = 14; i <30 ; i++) {
+            addObject(new Wall(), getPos(i), (getHeight() / 4) * 1 - halfWall);
+        }
+        //Bottom Left Vertical Wall
+        for (int i = 10; i < 16; i++) {
+            addObject(new Wall(), (getWidth() / 30) * 5 + halfWall, getPos(i));
+        }
+        // Middle Vertical Wall
+        for (int i = 5; i < 15; i++) {
+            addObject(new Wall(), (getWidth() / 30 ) * 14 + halfWall, getPos(i));
+        }
+        // Middle Left Horizontal Wall
+        for (int i = 6; i < 10; i++) {
+            addObject(new Wall(), getPos(i), getHeight() / 2 + halfWall);
+        }
+        //Long Bottom Wall Part 1
+        for (int i = 6; i < 15; i++) {
+            addObject(new Wall(), getPos(i), (getHeight() / 4) * 3 + halfWall);
+        }
+        //Long Bottom Wall Part 2
+        for (int i = 20; i < 30; i++) {
+            addObject(new Wall(), getPos(i), (getHeight() / 4) * 3 + halfWall);
+        }
+        //Middle Right Vertical Wall
+        for (int i = 11; i < 15; i++) {
+            addObject(new Wall(), (getWidth() / 30 ) * 20 + halfWall, getPos(i));
+        }
+        //Middle Far Right Vertical wall
+        for (int i = 5; i < 11; i++) {
+            addObject(new Wall(), (getWidth() / 30 ) * 25 + halfWall, getPos(i));
+        }
     }
 }
