6ca204731d54fe0832017104670a952cdb960b6c
Manasi Nagtode <nagtode62@gmail.com>
2014-10-30 11:58:09 -0700

    Bedroom 1, 2 and 3 Layout
    Layouts for Floor 2

---
+++ Bedroom3.java
@@ -0,0 +1,55 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Bedroom3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bedroom3 extends SaviourLevel
+{
+
+    /**
+     * Constructor for objects of class Bedroom3.
+     * 
+     */
+    public Bedroom3()
+    {
+        super(90, 70, 675, 200);
+        setWalls();
+    }
+    public void setWalls()
+    {
+        //top horiz right wall
+        for (int i = 1; i < 10; i++) {
+            addObject(new Wall(), getPos(i), getHeight() - 480 + halfWall);
+        }
+        //second left vert wall
+        for (int i = 4; i < 11; i++) {
+            addObject(new Wall(), getWidth() /3 + halfWall, getPos(i));
+        }
+        //left vert wall
+        for (int i = 8; i < 15; i++) {
+            addObject(new Wall(), getWidth() /6 + halfWall, getPos(i));
+        }
+        //third vert wall
+        for (int i = 1; i < 15; i++) {
+            addObject(new Wall(), getWidth() /2 + halfWall, getPos(i));
+        }
+        //bottom boriz wall
+         for (int i = 6; i < 25; i++) {
+            addObject(new Wall(), getPos(i), getHeight() - 180 + halfWall);
+        }
+        //right vert wall
+        for (int i = 5; i < 15; i++) {
+            addObject(new Wall(), getWidth() -180 + halfWall, getPos(i));
+        }
+        for (int i = 20; i < 25; i++) {
+            addObject(new Wall(), getPos(i), getHeight() - 480 + halfWall);
+        }
+         for (int i = 4; i < 11; i++) {
+            addObject(new Wall(), getWidth() /3 * 2 + halfWall, getPos(i));
+        }
+    }
+    
+}
