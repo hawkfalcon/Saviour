77bcf0550270a1c1c166ab62d1c1a4794c91e9d8
Shenpai <leighton.villanueva@yahoo.com>
2014-11-03 16:55:40 -0800

    Finished Kitchen 2 Layout
    Kitchen 2 Ready for obstacles.

--- Kitchen2.java
+++ Kitchen2.java
@@ -3,12 +3,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Kitchen2 here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Leighton made the walls
+ * Dane made the obstacles
+ * 
  */
 public class Kitchen2 extends SaviourLevel
 {
-
     /**
      * Constructor for objects of class Kitchen2.
      * 
@@ -16,5 +16,67 @@ public class Kitchen2 extends SaviourLevel
     public Kitchen2()
     {
         super(90, 70, 675, 200);
+        prepare();
+    }
+
+    public void prepare()
+    {
+        prepareWalls();
+        prepareObstacles();
+    }
+
+    //adds the walls
+    public void prepareWalls()
+    {
+        //generates all the walls
+        
+        //Top horizontal wall
+        for (int i = 1; i < 25; i++)
+        {
+            addObject(new Wall(), getPos(i),(getHeight() / 4) * 1 - halfWall);
+        }
+        //Bottom horizontal wall
+        for (int i = 5; i < 25; i++)
+        {
+            addObject(new Wall(), getPos(i),(getHeight() / 29) * 24 - halfWall);
+        }
+        //Left vertical wall
+        for (int i = 8; i < 15; i ++)
+        {
+            addObject(new Wall(), (getWidth() / 30) * 5 + halfWall, getPos(i));
+        }
+        //Mid left vertical wall
+        for(int i = 5; i < 12; i++)
+        {
+            addObject(new Wall(),(getWidth() / 30) * 10 + halfWall, getPos(i));
+        }
+        //Mid vertical wall
+        for(int i = 8; i < 15; i++)
+        {
+            addObject(new Wall(),(getWidth() / 30) * 15 + halfWall, getPos(i));
+        }
+        //Mid far right vertical wall
+        for(int i = 5; i < 12; i++)
+        {
+            addObject(new Wall(),(getWidth() / 30) * 20 + halfWall, getPos(i));
+        }
+        //Far Right Vertical wall
+        for (int i = 4; i < 15; i++) {
+            addObject(new Wall(), (getWidth() / 30 ) * 24 + halfWall, getPos(i));
+        }
+    }
+
+    //adds the obstacles
+    public void prepareObstacles()
+    {
+        //add spears
+        addObstacle(new Spear(), 165, 225, false, 270);
+        
+        //add acid
+        addObstacle(new Acid(), 50, 260);
+        addObstacle(new Acid(), 125, 375);
+        
+        //add arrow
+        
     }
 }
