15f44d030bc48d4055b053194b915b959e494604
Shenpai <leighton.villanueva@yahoo.com>
2014-11-04 09:34:13 -0800

    Finishing Up Kitchen 2 Obstacles
    Nearly complete

--- Kitchen2.java
+++ Kitchen2.java
@@ -3,8 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Kitchen2 here.
  * 
- * Leighton made the walls
- * Dane made the obstacles
+ * Leighton made the walls/obstacles 
  * 
  */
 public class Kitchen2 extends SaviourLevel
@@ -30,13 +29,23 @@ public class Kitchen2 extends SaviourLevel
     {
         //generates all the walls
         
-        //Top horizontal wall
-        for (int i = 1; i < 25; i++)
+        //Top horizontal wall(Left)
+        for (int i = 1; i < 12; i++)
         {
             addObject(new Wall(), getPos(i),(getHeight() / 4) * 1 - halfWall);
         }
-        //Bottom horizontal wall
-        for (int i = 5; i < 25; i++)
+        //Top horizontal wall(Right)
+        for (int i =13; i < 25; i++)
+        {
+            addObject(new Wall(), getPos(i), (getHeight() / 4) * 1 - halfWall);
+        }
+        //Left bottom horizontal wall
+        for (int i = 9; i < 25; i++)
+        {
+            addObject(new Wall(), getPos(i),(getHeight() / 29) * 24 - halfWall);
+        }
+        //Far left bottom horizontal wall
+        for (int i = 5; i < 8; i++)
         {
             addObject(new Wall(), getPos(i),(getHeight() / 29) * 24 - halfWall);
         }
@@ -71,12 +80,23 @@ public class Kitchen2 extends SaviourLevel
     {
         //add spears
         addObstacle(new Spear(), 165, 225, false, 270);
+        addObstacle(new Spear(), 315, 343, false, 90);
+        addObstacle(new Spear(), 465, 220, false, 270);
+        addObstacle(new Spear(), 615, 350, false, 90);
         
         //add acid
         addObstacle(new Acid(), 50, 260);
         addObstacle(new Acid(), 125, 375);
+        addObstacle(new Acid(), 725, 563);
+        addObstacle (new Acid(), 525, 484);
+        addObstacle(new Acid(), 325, 562);
+        addObstacle(new Acid(), 175, 485);
         
         //add arrow
-        
+        addObstacle(new Arrow(), 856, 526, true, 180);
+        addObstacle(new Arrow(), 89, 555, true, 270);
+        addObstacle(new Arrow(), 255, 465, true, 270);
+        addObstacle(new Arrow(), 375, 135, true, 90);
+        addObstacle(new Arrow(), 555, 465, true, 270);
     }
 }
