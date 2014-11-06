fe6df06f80b7880e24fb51134a463482985a3316
hawkfalcon <hawkfalcon1@gmail.com>
2014-11-06 09:06:15 -0800

    EVERYTHING IS TRANSPARENT. MWAHAHAHAAHA.

--- SaviourLevel.java
+++ SaviourLevel.java
@@ -53,9 +53,9 @@ public class SaviourLevel extends World {
     public SaviourLevel(int xStart, int yStart, int xEnd, int yEnd, LevelType type) {
         this(xStart, yStart, xEnd, yEnd);
         setBackground(new GreenfootImage(/*"/backgrounds/" +*/ type.toString().toLowerCase() + ".jpg"));
-        if (type == LevelType.DUNGEON) {
+        //if (type == LevelType.DUNGEON) {
             getBackground().setTransparency(200);
-        }
+      //  }
     }
 
     /**
