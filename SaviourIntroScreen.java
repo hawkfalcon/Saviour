59abe5de6e9f7bb77f056637c5be9e89fb10c5f2
Manasi Nagtode <nagtode62@gmail.com>
2014-11-24 19:56:46 -0800

    Intro Screen

--- SaviourIntroScreen.java
+++ SaviourIntroScreen.java
@@ -8,7 +8,8 @@ public class SaviourIntroScreen extends World {
     public SaviourIntroScreen() {
         // Create a new world with 900x600 cells with a cell size of 1x1 pixels.
         super(900, 600, 1);
-        showText("Click to play...", getWidth() / 2 - 40, getHeight() - 40);
+        showText("Start Game", getWidth() - 230, getHeight()/4 - 50);
+        prepare();
     }
 
     //restart game on click
@@ -17,5 +18,10 @@ public class SaviourIntroScreen extends World {
             SaviourWorld sw = new SaviourWorld();
             Greenfoot.setWorld(sw.getNextLevel());
         }
+        
+    }
+    public void prepare(){
+        addObject(new Character(), 150, 100);
+        addObject(new Endpoint(), 570, 100);
     }
 }
