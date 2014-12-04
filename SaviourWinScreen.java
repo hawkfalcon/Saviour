ac09bb5b9f59b02c41e87f10fb019e87522d6a91
hawkfalcon <hawkfalcon1@gmail.com>
2014-12-04 09:01:02 -0800

    NOW IS NOT A GOOD TIME TO HAVE MERGE ERRORS

--- SaviourWinScreen.java
+++ SaviourWinScreen.java
@@ -1,33 +1,8 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.awt.Color;
-/**
- * Write a description of class SaviourWinScreen here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class SaviourWinScreen extends World
-{
 
-<<<<<<< Updated upstream
 public class SaviourWinScreen extends World {
-    public SaviourWinScreen() {
-        // Create a new world with 900x600 cells with a cell size of 1x1 pixels.
-        super(900, 600, 1);
-        showText("YOU WIN!", getWidth() / 2, getHeight() / 2);
-        showText("Total Time:   " + GlobalStatistics.getInstance().getTimer(), getWidth() / 4, getHeight() / 4);
-    }
-
-    //restart game on click
-    public void act() {
-        if (Greenfoot.mouseClicked(this)) {
-            SaviourWorld sw = SaviourWorld.getInstance();
-            sw.levelup();
-            addObject(new Fade(sw.getNextLevel(false), true), getWidth() / 2, getHeight() / 2);
-            sw.reset();
-        }
-=======
-    /**
+   /**
      * Constructor for objects of class SaviourWinScreen.
      * 
      */
@@ -38,7 +13,15 @@ public class SaviourWinScreen extends World {
        
         showText("CONGRATULATIONS!", getWidth() /3 - 100, getHeight()/4 - 50);
         showText("YOU WON!", getWidth() /3 - 150, getHeight()/4);
-        
->>>>>>> Stashed changes
-    }
+        showText("Total Time:   " + GlobalStatistics.getInstance().getTimer(), getWidth() / 4, (getHeight() / 3)*2);
+    } 
+    //restart game on click
+    public void act() {
+        if (Greenfoot.mouseClicked(this)) {
+            SaviourWorld sw = SaviourWorld.getInstance();
+            sw.levelup();
+            addObject(new Fade(sw.getNextLevel(false), true), getWidth() / 2, getHeight() / 2);
+            sw.reset();
+        }
+    }    
 }
